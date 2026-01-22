7e7dc6f42ce919fe4b08f25fc78ea8d8d65d4e3e
Ani Georgieva <ageorgieva475@gmail.com>
2026-01-23 01:43:30 +0200

    Add files via upload

---
+++ ShooterSam/Source/ShooterSam/BTTask_BlackboardValue.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTTask_BlackboardValue.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+UBTTask_BlackboardValue::UBTTask_BlackboardValue()
+{
+	NodeName = TEXT("Clear Blackboard Value");
+}
+
+EBTNodeResult::Type UBTTask_BlackboardValue::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	Super::ExecuteTask(OwnerComp, NodeMemory);
+
+	UBlackboardComponent* Blackboard = OwnerComp.GetBlackboardComponent();
+
+	if (Blackboard)
+	{
+		Blackboard->ClearValue(GetSelectedBlackboardKey());
+	}
+
+	return EBTNodeResult::Succeeded;
+}
---
+++ ShooterSam/Source/ShooterSam/BTTask_BlackboardValue.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "BTTask_BlackboardValue.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTERSAM_API UBTTask_BlackboardValue : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+
+
+public:
+	UBTTask_BlackboardValue();
+
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
